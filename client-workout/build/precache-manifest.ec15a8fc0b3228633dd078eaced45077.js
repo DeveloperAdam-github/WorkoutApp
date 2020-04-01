6ce17fd926ffac4b879b40bf767099a34100c37e
Adam Powell <adampowell@adams-imac.home>
2020-04-01 20:01:58 +0100

    ready to deploy

---
+++ client-workout/build/precache-manifest.ec15a8fc0b3228633dd078eaced45077.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "980e0b7fdc5b71f7a6110418d90ecc83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4259960b13fc95cba1da",
+    "url": "/static/css/main.fda96898.chunk.css"
+  },
+  {
+    "revision": "668aec6eca957df5cbfd",
+    "url": "/static/js/2.eafdb018.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.eafdb018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4259960b13fc95cba1da",
+    "url": "/static/js/main.8a4870a3.chunk.js"
+  },
+  {
+    "revision": "3226de76c210e8134fc3",
+    "url": "/static/js/runtime-main.af68c3be.js"
+  },
+  {
+    "revision": "28dbb0637f1156e069239386c4e02900",
+    "url": "/static/media/FitnessImage1.28dbb063.svg"
+  },
+  {
+    "revision": "eb542ee89098ae9eccf5235e592df7fb",
+    "url": "/static/media/Jimwendler.eb542ee8.jpg"
+  },
+  {
+    "revision": "ec45de4923761733f357fc0619d9e954",
+    "url": "/static/media/adrien-olichon-RCAhiGJsUUE-unsplash.ec45de49.jpg"
+  },
+  {
+    "revision": "650cdc57dc0be3a71cc230a760215df5",
+    "url": "/static/media/bin.650cdc57.svg"
+  },
+  {
+    "revision": "2927f8de4268bae8f11c4dfe3adc72b5",
+    "url": "/static/media/edit.2927f8de.svg"
+  },
+  {
+    "revision": "7b145fe41b026c6db2ba57fa16127023",
+    "url": "/static/media/gym.7b145fe4.svg"
+  },
+  {
+    "revision": "efa2f1bd24d1dc22a0a85c0f025b0309",
+    "url": "/static/media/logout.efa2f1bd.svg"
+  },
+  {
+    "revision": "d0f5911d9a71bc7ec61eff8a4f25ea4e",
+    "url": "/static/media/plus.d0f5911d.svg"
+  },
+  {
+    "revision": "378d0df42d41540553cd43b8484f67ba",
+    "url": "/static/media/spinner.378d0df4.gif"
+  }
+]);
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,12 +10,22 @@ connectDB();
 //INIT MIDDLEWARE
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to your workouts' }));
-
 // DEFINE OUR ROUTES
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/workouts', require('./routes/workouts'));
+
+// Serve stat assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client-workout/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, 'client-workout', 'build', 'index.html')
+    )
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
